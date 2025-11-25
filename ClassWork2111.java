bd1cf3ec8e44a210096184280e42bb01a3b1365e
ext <68648123+unplaged@users.noreply.github.com>
2025-11-25 22:33:08 +0300

    Add files via upload

---
+++ ClassWork2111.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ClassWork2111 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+
+        int fact = 1;
+        for (int i = 1; i <= x; i++){
+            fact = fact * i;
+        }
+        System.out.println(fact);
+    }
+
+}
