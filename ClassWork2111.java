b1bdd66f0b152b5035baee91e9b8ed25ac2bca71
ext <68648123+unplaged@users.noreply.github.com>
2025-11-25 22:43:11 +0300

    HomeWork_21.11

---
+++ ClassWork2111.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ClassWork2111 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        int fact = 1;
+        for (int i = 1; i <= N; i++){
+            fact = fact * i;
+        }
+        System.out.println(fact);
+    }
+
+}
